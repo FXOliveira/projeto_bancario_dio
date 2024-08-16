695f27ed4d41265516736c7af458b36d99ae2d12
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2024-08-16 07:52:13 -0300

    implementação de exceção personalizado para em caso de erro nas operações bancárias

---
+++ bank/src/main/java/com/felipe/project/bank/exceptions/ErroOperacaoBancariaException.java
@@ -0,0 +1,7 @@
+package com.felipe.project.bank.exceptions;
+
+public class ErroOperacaoBancariaException extends Exception {
+    public ErroOperacaoBancariaException(String message){
+        super("message");
+    }
+}
