fd44ee71f3d67323571848c60072f2e5bd48b79f
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2024-08-16 07:30:41 -0300

    remoção classe antigas

--- bank/src/main/java/com/felipe/project/bank/repository/ContaPoupancaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felipe.project.bank.repository;
-
-import com.felipe.project.bank.model.ContaPoupanca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContaPoupancaRepository extends JpaRepository<ContaPoupanca, Long> {
-}
