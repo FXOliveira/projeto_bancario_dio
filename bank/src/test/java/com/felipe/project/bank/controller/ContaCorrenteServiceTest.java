660250ada00047957dbf85a5561be167768e945b
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2024-10-11 15:47:17 -0300

    teste unitario para o metodo de saque lançar exceção quando incluir valor inválido

--- bank/src/test/java/com/felipe/project/bank/controller/ContaCorrenteServiceTest.java
+++ bank/src/test/java/com/felipe/project/bank/controller/ContaCorrenteServiceTest.java
@@ -1,6 +1,7 @@
 package com.felipe.project.bank.controller;
 
 import com.felipe.project.bank.exceptions.ErroOperacaoBancariaException;
+import com.felipe.project.bank.model.Conta;
 import com.felipe.project.bank.model.ContaCorrente;
 import com.felipe.project.bank.service.ContaCorrenteService;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class ContaCorrenteServiceTest {
@@ -20,6 +23,7 @@ class ContaCorrenteServiceTest {
     void setUp() {
         contaCorrente = new ContaCorrente();
         contaCorrente.setId(1L);
+        contaCorrente.setSaldo(10000.0);
         contaCorrente.setTitular("fulano");
         contaCorrente.setAgencia("agencia x");
     }
@@ -28,11 +32,22 @@ class ContaCorrenteServiceTest {
         Mockito.doThrow(new ErroOperacaoBancariaException("Valor de depósito inválido"))
                 .when(contaCorrenteService).depositar(1L, -100.0);
 
-        Assertions.assertThrows(ErroOperacaoBancariaException.class, () ->
+        assertThrows(ErroOperacaoBancariaException.class, () ->
                 contaCorrenteService.depositar(1L, -100.0));
 
         Mockito.verify(contaCorrenteService).depositar(1L, -100.0);
 
     }
+    @Test
+    void saque_WhenInvalidValue_ThenThrowException() throws ErroOperacaoBancariaException {
+
+        Mockito.doThrow(new ErroOperacaoBancariaException("Valor de saque inválido!!"))
+                .when(contaCorrenteService).sacar(1L, -100.0);
+
+        Mockito.doThrow(new ErroOperacaoBancariaException("Valor de saque inválido!!"))
+                .when(contaCorrenteService).sacar(1L, 11000.0);
+
+    }
+
 
 }
