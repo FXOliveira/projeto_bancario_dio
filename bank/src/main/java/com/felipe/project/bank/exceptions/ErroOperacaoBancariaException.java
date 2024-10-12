48a4314424ca760f6006f0a53e26d33f0a869e9b
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2024-10-12 17:13:32 -0300

    ajuste na exceção personalizada ErroOperacaoBancariaException

--- bank/src/main/java/com/felipe/project/bank/exceptions/ErroOperacaoBancariaException.java
+++ bank/src/main/java/com/felipe/project/bank/exceptions/ErroOperacaoBancariaException.java
@@ -2,6 +2,6 @@ package com.felipe.project.bank.exceptions;
 
 public class ErroOperacaoBancariaException extends Exception {
     public ErroOperacaoBancariaException(String message){
-        super("message");
+        super(message);
     }
 }
